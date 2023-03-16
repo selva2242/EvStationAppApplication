c4caee45e508e613a1660785d70ae30fa75ad278
Selva <selva2242@gmail.com>
2023-03-16 20:02:04 +0530

    Initialised EV app project

---
+++ src/main/java/com/example/EVStationApp/EvStationAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.EVStationApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvStationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvStationAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EVStationApp/EvStationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EVStationApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvStationAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
